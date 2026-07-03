07c525cbff29719e82fee2b94941314f72f384be
Mateusz Król <127255885+baramundi666@users.noreply.github.com>
2026-07-03 15:54:55 +0200

    Upload changes

--- src/starlink/getstat.sh
+++ src/starlink/getstat.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/bash
 
-make
-./main_static_network > 2mpxcp_out.txt
-../parse_output mpxcp1.log -ascii > 2mpxcp_out_log.txt
-cat 2mpxcp_out_log.txt | grep XCP_SINK > temp2mpxcp.txt
+# make
+# ./main_static_network > 2mpxcp_out.txt
+../parse_output starlink1.log -ascii > 2ss.txt
+cat 2ss.txt | grep XCP_SINK > temp2ss.txt
